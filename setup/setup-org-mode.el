becded23a121f840a911583308d7b2a3702cb847
Stuart MacKay <smackay@flagstonesoftware.com>
2019-07-14 09:50:27 +0100

    Renamed refile target function

--- setup/setup-org-mode.el
+++ setup/setup-org-mode.el
@@ -72,11 +72,11 @@
 (setq org-indirect-buffer-display 'current-window)
 
 ;; Exclude DONE state tasks from refile targets
-(defun bh/verify-refile-target ()
+(defun verify-refile-target ()
   "Exclude todo keywords with a done state from refile targets"
   (not (member (nth 2 (org-heading-components)) org-done-keywords)))
 
-(setq org-refile-target-verify-function 'bh/verify-refile-target)
+(setq org-refile-target-verify-function 'verify-refile-target)
 
 ;;;; Org Log
 
