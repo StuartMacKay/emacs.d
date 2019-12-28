5803050afda6c452bd835cccf5caf2530d849d4f
Stuart MacKay <smackay@flagstonesoftware.com>
2019-12-28 21:08:28 +0000

    Configured autocomplete and slime.

--- setup/setup-modes.el
+++ setup/setup-modes.el
@@ -20,4 +20,19 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mdown$" . markdown-mode))
 
+;;; Setup slime mode and autocomplete
+;;; from https://github.com/purcell/ac-slime
 
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
+
+
+;;; Select the modes where autocomplete is enabled
+
+(ac-config-default)
+
+(add-to-list 'ac-modes 'python-mode)
+(add-to-list 'ac-modes 'js2-mode)
+(add-to-list 'ac-modes 'web-mode)
